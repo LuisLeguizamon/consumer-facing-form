f0e65449c40726675efb564265a31f7da1b79513
Luis Leguizamón <lleguizamonl@gmail.com>
2024-03-11 18:25:26 -0300

    New: Test client form can be rendered

--- tests/Feature/ClientFormTest.php
+++ tests/Feature/ClientFormTest.php
@@ -5,12 +5,12 @@ namespace Tests\Feature;
 // use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class ExampleTest extends TestCase
+class ClientFormTest extends TestCase
 {
     /**
      * A basic test example.
      */
-    public function test_the_application_returns_a_successful_response(): void
+    public function test_client_form_can_be_rendered(): void
     {
         $response = $this->get('/');
 

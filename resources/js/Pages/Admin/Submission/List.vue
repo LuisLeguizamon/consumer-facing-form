91405af0cc6595424ff53c665a1b7ebef0a59f86
Luis Leguizamón <lleguizamonl@gmail.com>
2024-03-11 16:52:04 -0300

    Style: Change submissions table style

--- resources/js/Pages/Admin/Submission/List.vue
+++ resources/js/Pages/Admin/Submission/List.vue
@@ -19,19 +19,32 @@ defineProps({
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <table>
-                        <thead>
-                            <td>
-                                Created At
-                            </td>
+                    <table class="min-w-full divide-y divide-gray-200">
+                        <thead class="bg-gray-50">
+                        <tr>
+                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                Date/Time
+                            </th>
+                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                Product
+                            </th>
+                        </tr>
                         </thead>
-
-                        <tbody>
-                            <tr v-for="submission in submissions">
-                                <td>
-                                    {{ submission.created_at }}
+                        <tbody class="bg-white divide-y divide-gray-200">
+                            <tr v-for="submission in submissions" :key="submission.id">
+                                <td class="px-6 py-4">
+                                    <div class="text-sm text-gray-900">{{ submission.created_at }}</div>
+                                </td>
+                                <td class="px-6 py-4 whitespace-nowrap">
+                                    <div class="text-sm text-gray-900">{{ submission.insurance_product }}</div>
                                 </td>
                             </tr>
+                            <tr>
+                                <td class="px-6 py-4 whitespace-nowrap" colspan="5" v-if="submissions.length === 0">
+                                    <div class="text-sm text-gray-900 text-center">No submissions founded.</div>
+                                </td>
+                            </tr>
+
                         </tbody>
                     </table>
                 </div>

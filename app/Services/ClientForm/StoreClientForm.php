57d2457de29f03fdd893384a97db263afdf17a56
Luis Leguizamón <lleguizamonl@gmail.com>
2024-03-11 19:58:19 -0300

    Refactor: Implement a transaction to store client form information

--- app/Services/ClientForm/StoreClientForm.php
+++ app/Services/ClientForm/StoreClientForm.php
@@ -4,21 +4,33 @@ namespace App\Services\ClientForm;
 
 use App\Services\Consumer\CreateConsumer;
 use App\Services\Submission\CreateSubmission;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class StoreClientForm
 {
     public function __construct(
         private CreateConsumer $createConsumer,
         private CreateSubmission $createSubmission
-    )
-    {}
+    ) {
+    }
 
     public function execute(array $validated)
     {
-        $consumer = $this->createConsumer->execute($validated);
+        DB::beginTransaction();
 
-        foreach ($validated['products'] as $product) {
-            $this->createSubmission->execute($consumer->id, $product);
+        try {
+            $consumer = $this->createConsumer->execute($validated);
+
+            foreach ($validated['products'] as $product) {
+                $this->createSubmission->execute($consumer->id, $product);
+            }
+
+            DB::commit();
+
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            Log::error($th->getMessage());
         }
     }
 }

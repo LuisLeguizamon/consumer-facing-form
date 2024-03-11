aa884decd2bc07ede09228db5eba8fbf3b6b9e23
Luis Leguizamón <lleguizamonl@gmail.com>
2024-03-11 08:12:02 -0300

    New: Implement Client Form for step 1

--- resources/js/Pages/ClientForm/ClientForm.vue
+++ resources/js/Pages/ClientForm/ClientForm.vue
@@ -1,22 +1,33 @@
 <script setup>
+import { ref } from 'vue';
+import { useForm } from '@inertiajs/vue3';
+import ClientFormStep1 from './Partials/ClientFormStep1.vue';
+
 defineProps({
     'products': Object
 });
+
+const form = useForm({
+    _method: 'POST',
+    products: [],
+});
+
+let showStep1 = ref(true);
+
+function productSelected(value) {
+// TODO
+};
+
+function nextStep() {
+// TODO
+}
 </script>
 <template>
-    <div class="grid md:grid-cols-2 bg-white">
-        <div>
-            <img src="/images/image_form_1.jpeg" alt="Image form 1">
-        </div>
-        <div class="ml-12 mr-24 mt-6">
-            <!-- Product Cards -->
-            <div
-                v-for="(product, index) in products"
-                class="p-6 mt-6"
-                :style="{ backgroundImage: 'url(/images/image_form_1.jpeg)' }"
-            >
-                <h1>{{ product }}</h1>
-            </div>
-        </div>
-    </div>
+    <ClientFormStep1
+        v-show="showStep1"
+        :products="products"
+        :products-selected="form.products"
+        @product-selected="productSelected($event)"
+        @next-step="nextStep()"
+    />
 </template>
---
+++ resources/js/Pages/ClientForm/Partials/ClientFormStep1.vue
@@ -0,0 +1,40 @@
+<script setup>
+import ProductCard from './ProductCard.vue';
+
+defineEmits(['next-step', 'product-selected']);
+
+const props = defineProps({
+    'products': Object,
+    'productsSelected': Object,
+});
+
+function isProductSelected(value) {
+    return props.productsSelected.includes(value);
+}
+
+</script>
+<template>
+    <div class="grid md:grid-cols-2 bg-white h-screen">
+        <div class="bg-contain bg-no-repeat m-0.5 rounded-xl"
+             style="background-image: url('/images/image_form_1.jpeg'); background-position: center;">
+        </div>
+        <div class="mr-24 mt-6">
+            <h1 class="font-bold text-slate-800 text-xl">
+                Let's get started
+            </h1>
+            <p class="text-slate-500 text-sm">
+                Please select one or more options below that you'd like to have quoted for insurance.
+            </p>
+            <!-- TODO: IMPLEMENT PRODUCT CARD -->
+            <button class="bg-blue-600 my-5 py-3 rounded-md text-white text-sm w-full"
+                    @click="$emit('next-step')">
+                Agree and Continue
+            </button>
+            <p class="text-slate-500 text-sm">
+                By continuing this form you agree to share data with Frankle
+                Insurance Services as defined in the Terms
+                of Use and Privacy Policy
+            </p>
+        </div>
+    </div>
+</template>
